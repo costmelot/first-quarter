4e14bc3d7ebeac5fecae4ea493fa3f45d0389a8e
Andrey <costmelot@mail.ru>
2022-09-29 17:01:06 +0400

    Добавлены все примеры для задачи

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+﻿string[] array1 = new string[11] {"hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Kazan", "Denmark"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
